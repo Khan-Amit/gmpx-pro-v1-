a573484c55456dc656190715cac4e44affc066b9
ENKI <amit.khanna.1082@gmail.com>
2026-03-20 15:37:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    GOLD_API = os.getenv("API_KEY_GOLD")
-    FRED_API = os.getenv("API_KEY_FRED")
-    NEWS_API = os.getenv("API_KEY_NEWS")
-
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
     DATA_PATH = "data/raw/"
     LOG_PATH = "logs/"
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
